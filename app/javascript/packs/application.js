1863425156594dcc74f67526c87a1db2165b4190
Ilana Seidl <iseidl@tulane.edu>
2019-12-11 12:37:23 -0600

    Add bootstrap form gem.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
 //= require jquery
 //= require bootstrap-sprockets
+//=require popper
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
