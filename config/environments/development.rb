546c5ef090c23a7d496f3236e0e3bd1099c40916
mcaplin98 <mcaplin98@gmail.com>
2019-12-11 21:47:31 +0000

    bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "9376171aac4d4c7194bf59af58862334.vfs.cloud9.us-east-2.amazonaws.com"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
