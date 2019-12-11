6b8c3b342b8870425202bf1b83712f5fabcdeeed
mcaplin98 <mcaplin98@gmail.com>
2019-12-11 21:27:47 +0000

    fixed cucumber

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -3,17 +3,24 @@ module SignUpStepHelper
     @user = User.new(email: email, counter: 0)
     @user.save
   end
+  def counter
+    @total = User.all.count
+  end
 end
 World SignUpStepHelper
 
 Given("current user's email") do
+  @first_total = counter
   sign_up("email@example.com")
 end
 
 When("user successfully sign up") do
-  @actual_answer = User.find_by(email: @email).present?
+  @second_total = counter
+  @actual_answer = User.find_by(email: "email@example.com").present?
 end
 
-Then("user appears in the db}") do
-  assert_equal @actual_answer, 'true'
+Then("user appears in the db") do
+  #puts(@actual_answer)
+  assert_equal @actual_answer, true
+  assert_equal @second_total, @first_total+1
 end
