243434b568d3fe973b8c37b3011fca5f78625358
Joel Hochman <hochmanjoel@gmail.com>
2019-12-12 04:26:04 +0000

    updated stepdefs

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -16,11 +16,8 @@ end
 
 When("user successfully sign up") do
   @second_total = counter
-  @actual_answer = User.find_by(email: "email@example.com").present?
 end
 
 Then("user appears in the db") do
-  #puts(@actual_answer)
-  assert_equal @actual_answer, true
   assert_equal @second_total, @first_total+1
 end
