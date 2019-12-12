45310d9bc2a3457c29822f6992876fd74757731a
mcaplin98 <mcaplin98@gmail.com>
2019-12-12 04:24:15 +0000

    setup working on it

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,3 +1,4 @@
 class Referral < ApplicationRecord
   belongs_to :user, foreign_key: :referrer, foreign_key: :referred
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :referrals
-  validates :email, presence: true, 'valid_email_2/email': true
+
+  if validates :email, presence: true, length: {maximum: 100},allow_nil: false, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  else
+    puts "invalid email"
+  end
 end
